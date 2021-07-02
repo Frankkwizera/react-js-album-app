6bd9631401045b272585a8d5c2c14ceb340db889
Frank Kwizera <frankpmn@gmail.com>
2021-07-03 00:26:05 +0200

    Added md xs width

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Button, Input } from "reactstrap";
+import { Button, Input, Card } from "reactstrap";
 import { Container, Row, Col } from "reactstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
@@ -15,7 +15,7 @@ class App extends React.Component {
     super();
     this.state = { 
       value: null,
-      photos: [1, 2, 3, 4, 5, 6, 7],
+      photos: [],
     };
   };
 
@@ -24,18 +24,26 @@ class App extends React.Component {
   }
 
   retrieveAlbumPhotos = () => {
+    if (!this.state.value) {
+      alert('Input the album ID.');
+      return;
+    }
     const albumPhotosEndpoint = this.backend_enpoint + this.state.value + "/photos";
-    console.log("<<<<<< albumPhotosEndpoint ", albumPhotosEndpoint);
     fetch(albumPhotosEndpoint)
       .then(response => response.json())
       .then(
         (albumPhotos) => {
-          console.log("<<<<< backend response: ", albumPhotos)
           this.setState({
             photos: albumPhotos
           });
+          if (this.state.photos.length === 0) {
+            alert('No album photos for album id ' + this.state.value);
+          }
         },
         (error) => {
+          this.setState({
+            photos: []
+          });
           console.error("Error: ", error);
         }
       )
@@ -57,9 +65,11 @@ class App extends React.Component {
         </div>
         <Row className="album-photos">
           {this.state.photos.map((photo) => (
-            <Col className="single-photo col" sm="4">
+            <Col className="single-photo col" xs="6" sm="4" md="3">
+              <Card>
               <img className="album-photo" width="100%" src={photo.thumbnailUrl} alt={photo.url} />
               <p className="image-title">{photo.title}</p>
+              </Card>
             </Col>
             ))}
         </Row>
